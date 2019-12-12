b47c81731953a0f9f37d08210a54aadadcba8f57
Riouallon Vincent <riouallonvincent@gmail.com>
2019-12-12 20:19:37 +0100

    chore: change isSigned function

--- src/testConnection.ts
+++ src/testConnection.ts
@@ -1,12 +1,13 @@
-import errors from 'errors'
+// import errors from 'errors'
 import request from'./request'
 
-export function isSignedIn(options: any) {
-    return request({ url: 'https://www.ticketswap.fr/profile', session: options.sessionID })
+export function isSignedIn() {
+    return request('https://www.ticketswap.fr/profile')
         .then((result) => {
             // If we were redirected to the login page the session id wasn't valid
             if (result.response.request.uri.path === '/login') {
-                throw new errors.NotSignedInError(`The session id ${options.sessionID} is not valid`);
-            }
+              console.error('error')
+                // throw new errors.NotSignedInError(`The session id ${options.sessionID} is not valid`);
+            }else console.log('ok')
         });
 }
