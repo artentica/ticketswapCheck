4162dc775376bf1d13478503457d014860c9c955
Riouallon Vincent <riouallonvincent@gmail.com>
2019-12-19 20:19:37 +0100

    chore: add import

--- src/foundTickets.ts
+++ src/foundTickets.ts
@@ -1,6 +1,11 @@
 import * as  notifier from 'node-notifier'
 import * as chalk from 'chalk'
 import * as cheerio from 'cheerio'
+import {exec} from 'child_process'
+
+import request from './request'
+import logger from './logger'
+// const utils = require('./utils');
 
 export function parseHTML(result) {
 
