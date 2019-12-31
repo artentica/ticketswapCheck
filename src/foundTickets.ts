a32a4b48435e29d68e6dd906dbe9158ee743e308
Riouallon Vincent <riouallonvincent@gmail.com>
2019-12-31 16:31:40 +0100

    begin run found and parse html

--- src/foundTickets.ts
+++ src/foundTickets.ts
@@ -7,8 +7,52 @@ import logger from './logger'
 import request from './request'
 // const utils = require('./utils');
 
-export function parseHTML($: object) {}
-
 export function reserve({ form, csrf, availableTickets, _endpoint }, link, options) {}
 
-export async function runFound(link, options): Promise<object> {}
+export async function runFound(link, options): Promise<object> {
+  // STEP 1 submit form
+  // STEP 2 request /cart
+
+  return request(link).then(parseHTML)
+  // .then(result => process(result, link, options))
+  // .then((result) => {
+  //     if (result.alreadySold) {
+  //         return result;
+  //     }
+
+  //     notifier.notify({
+  //         title: 'TicketScoop!',
+  //         message: 'Found a ticket, now opening your cart!',
+  //         sound: true,
+  //     });
+
+  //     exec('open -a "Google Chrome" https://www.ticketswap.nl/cart');
+
+  //     return {
+  //         alreadySold: false,
+  //     };
+  // });
+}
+
+export function parseHTML($: any) {
+  // var form = $('#listing-reserve-form');
+  // var _endpoint = form.data('endpoint');
+  // var csrf = $('meta[name="csrf_token"]')[0].attribs.content;
+  // let availableTickets = 0;
+  // form.find('select[name="amount"] option').each(function(_i, elem) {
+  //     let value = $(elem).attr('value');
+  //     value = parseInt(value, 10);
+  //     if (isNaN(value)) {
+  //         throw new TypeError('Expected option.value to be of type number');
+  //     }
+  //     if (value > availableTickets) {
+  //         availableTickets = value;
+  //     }
+  // });
+  // return {
+  //     form,
+  //     _endpoint,
+  //     availableTickets,
+  //     csrf,
+  // };
+}
