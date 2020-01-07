57b163056d20830a182b68dc3262e935698b9aea
Riouallon Vincent <riouallonvincent@gmail.com>
2020-01-07 12:00:00 +0100

    chore: continue main

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
 import * as chalk from 'chalk'
+import * as notifier from 'node-notifier'
+import * as opn from 'opn'
 
 import config from './config'
 import logger from './logger'
 import request from './request'
 import Parser from './parser'
-import {NoTicketsFoundError} from './error'
+import { NoTicketsFoundError, DepletedTicketsError } from './error'
 import { runFound } from './foundTickets'
 
 class Main {
   private lastDateRequest: number
+  private amountReserved: number
 
   constructor() {
     this.lastDateRequest = Date.now()
+    this.amountReserved = 0
   }
 
-  public checkIfTicketsAvailable = parser => {
+  private checkIfTicketsAvailable = parser => {
     if (parser.ticketsAvailable.length === 0) {
       logger.info(chalk.blue('No tickets found!'), Date.now() - this.lastDateRequest, 'ms')
       this.lastDateRequest = Date.now()
@@ -30,19 +34,51 @@ class Main {
     }
   }
 
-  public buyIfFound = (options, { found, parser }) => {
+  private buyIfFound = (options, { found, parser }) => {
     const ticket = parser.popTicket()
     if (found && ticket) {
-      return runFound(ticket.link, options).then(_result => {
-        //console.log(result)
-        // if (result && result.alreadySold) {
-        //     return tryNextTicket(options, parser);
-        // }
+      return runFound(ticket.link, { ...options, amount: options.amount - this.amountReserved }).then(result => {
+        // if an amount have been bought we subtract of the total desire
+        if (result.amountOfTickets) this.amountReserved += result.amountOfTickets
+        // if nothing have been bought or if we still have
+        if (options.amount - this.amountReserved !== 0) {
+          return this.tryNextTicket(options, parser)
+        }else {
+          this.notifyIfTicketReserved(options)
+        }
 
         // return result;
       })
     } else {
       return Promise.reject(new NoTicketsFoundError('Found no tickets to buy'))
+    }
+  }
+
+  private tryNextTicket = (options, parser) => {
+    if (parser.haveAnotherTicket()) {
+      logger.info('Found another potential ticket')
+
+      return this.buyIfFound(options, { found: true, parser })
+    } else {
+      logger.info('Depleted all available tickets.')
+      logger.info('Restarting monitor.')
+      return Promise.reject(new DepletedTicketsError('None of the tickets is available'))
+    }
+  }
+
+  private notifyIfTicketReserved = (options) => {
+    if(this.amountReserved){
+      notifier.notify(
+        {
+          title: `TicketSwap ${this.amountReserved} ticket(s) reserved`,
+          message: 'Click to open browser',
+          sound: 'ding.mp3',
+          wait: true
+        },
+        function() {
+          opn(options.baseUrl + '/cart')
+        }
+      )
     }
   }
 
@@ -54,7 +90,7 @@ class Main {
       .then(res => new Parser(config, res.body))
       .then(this.checkIfTicketsAvailable)
       .then(this.buyIfFound.bind(null, options))
-    // .catch(runCatchHandler.bind(null, options));
+    .catch(runCatchHandler.bind(null, options))
   }
 }
 
