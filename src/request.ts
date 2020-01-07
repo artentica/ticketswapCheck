4ad7716350e8d710bdbd9153f9a1a70214e6b8af
Riouallon Vincent <riouallonvincent@gmail.com>
2020-01-07 12:00:00 +0100

    chore: format request

--- src/request.ts
+++ src/request.ts
@@ -23,21 +23,24 @@ export default function request(url: string, opts: any = {}, cookies = []): Prom
     )
     .forEach(cookie => jar.setCookie(cookie.toString(), url))
 
-  const options = Object.assign({
-    uri: url,
-    jar,
-    method: 'GET',
-    headers: opts.headers || {
-      'user-agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
-    },
-    transform(body: any, response: any) {
-      return {
-        response,
-        body: cheerioExtended.load(body)
+  const options = Object.assign(
+    {
+      uri: url,
+      jar,
+      method: 'GET',
+      headers: opts.headers || {
+        'user-agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+      },
+      transform(body: any, response: any) {
+        return {
+          response,
+          body: cheerioExtended.load(body)
+        }
       }
-    }
-  }, opts)
+    },
+    opts
+  )
 
   logRequest(url, options)
 
