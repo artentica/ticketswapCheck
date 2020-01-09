2224ce61ec7f9f39feb35767a23b735a1ef06b77
Riouallon Vincent <riouallonvincent@gmail.com>
2020-01-09 14:41:33 +0100

    chore: reAdd config

---
+++ src/config.ts
@@ -0,0 +1,35 @@
+export default {
+  cookie: [
+    {
+      key: 'cookieAccepted',
+      value: 'cookieAccepted'
+    },
+    {
+      key: 'userId',
+      value: ''
+    },
+    {
+      key: 'geoInfo',
+      value: '%7B%22latitude%22%3A%2247.73560%22%2C%22longitude%22%3A%22-3.23050%22%2C%22countryCode%22%3A%22FR%22%7D'
+    },
+    {
+      key: 'sid',
+      value: 'C2-CyZILVAgVkDA4QJrEQ'
+    },
+    {
+      key: 'session',
+      value: ''
+    },
+    {
+      key: 'token',
+      value: ''
+    }
+  ],
+  retryPolicy: {
+    delay: [900, 1100],
+    retries: -1
+  },
+  url: '',
+  amount: 1,
+  logLevel: 1
+}
